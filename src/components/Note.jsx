7b5af1a8c3f97e0c0c6e4a1baadc9145102cce17
eduardo reta rj <https://github.com/Eddie-Reta>
2021-01-06 20:24:02 -0500

    App now runnung with First Note-Stack Theme.

---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import react from "react";
+
+function Note() {
+
+    let title = "";
+    let content = "";
+
+    return(
+        <div className="note">
+            <h1>This is the Note title</h1>
+            <p>This is the Note content</p>
+        </div>
+    );
+};
+
+export default Note;
